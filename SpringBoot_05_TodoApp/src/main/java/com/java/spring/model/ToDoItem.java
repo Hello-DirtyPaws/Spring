3a835bbb12f97f6c0a0a12c8121bc6ac26a4c820
Hello World <surajupadhyay.ru@gmail.com>
2022-02-24 10:20:24 -0500

    Enable Logback Loggings

--- SpringBoot_05_TodoApp/src/main/java/com/java/spring/model/ToDoItem.java
+++ SpringBoot_05_TodoApp/src/main/java/com/java/spring/model/ToDoItem.java
@@ -6,13 +6,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
 @Entity
 @Table(name = "Items")
 public class ToDoItem
 {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String name;
 	
@@ -43,7 +42,27 @@ public class ToDoItem
 	{
 		this.name = name;
 	}
-	
+
+	@Override
+	public int hashCode()
+	{
+		// Hashcode is based on the ID itself
+		return ((Integer) this.id).hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj != null && obj instanceof ToDoItem)
+		{
+			ToDoItem item = (ToDoItem) obj;
+
+			// Return true if the ID and Name both are equal, else false
+			return (this.id == item.id && this.name.equals(item.name));
+		}
+		return false;
+	}
+
 	public String toString()
 	{
 		return "id: " + this.id + ", name: " + this.name;
